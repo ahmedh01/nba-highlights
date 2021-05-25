182b28fc491c0828ce1e6cfebd275dc05194a10b
Ahmed Hashim <ahashim@gmail.com>
2021-05-25 10:15:28 +0500

    get highlights feed from reddit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
     <h1 class="mb-6 text-3xl font-extrabold">NBA Highlights</h1>
+
+    <div v-for="video in videos" :key="video.name">
+        <a :href="video.url" target="blank">
+            {{ video.title }}
+        </a>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+const store = useStore()
+
+store.dispatch('getHighlights')
+
+const videos = computed(() => {
+    return store.state.highlights.videos
+})
+</script>
--- src/store/modules/highlights.js
+++ src/store/modules/highlights.js
@@ -1,6 +1,6 @@
 export default {
     state: () => ({
-        name: 'Buster',
+        videos: [],
     }),
 
     getters: {
@@ -8,14 +8,33 @@ export default {
     },
 
     mutations: {
-        SET_NAME(state, payload) {
-            state.name = payload
+        SET_VIDEOS(state, payload) {
+            state.videos.push(...payload)
         },
     },
 
     actions: {
-        saveName({ commit }, data) {
-            commit('SET_NAME', data)
+        getHighlights({ commit }) {
+            let videos = []
+            let endpoint = `https://www.reddit.com/r/nba/search.json?q=flair%3AHighlight&restrict_sr=on&sort=new&t=all&limit=20`
+
+            fetch(endpoint)
+                .then((response) => response.json())
+                .then((data) => {
+                    data.data.children.forEach((item) => {
+                        videos.push({
+                            name: item.data.name,
+                            title: item.data.title,
+                            url: item.data.url,
+                            thumbnail: item.data.secure_media
+                                ? item.data.secure_media.oembed.thumbnail_url
+                                : '',
+                        })
+                    })
+                    commit('SET_VIDEOS', videos)
+                    console.log(videos)
+                })
+                .catch((error) => console.error('Error getting feed: ', error))
         },
     },
 }
